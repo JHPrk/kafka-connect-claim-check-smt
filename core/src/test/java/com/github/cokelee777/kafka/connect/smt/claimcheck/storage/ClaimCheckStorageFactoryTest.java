77379d419e46a2c4eb6d02f47aa3533def3eaef8
이채민 <chaemin.lee01@lotte.net>
2026-01-22 16:24:41 +0900

    [chore] ClaimCheckStorageFactory 단위 테스트 효율적으로 변경
    #40

--- core/src/test/java/com/github/cokelee777/kafka/connect/smt/claimcheck/storage/ClaimCheckStorageFactoryTest.java
+++ core/src/test/java/com/github/cokelee777/kafka/connect/smt/claimcheck/storage/ClaimCheckStorageFactoryTest.java
@@ -1,41 +1,59 @@
 package com.github.cokelee777.kafka.connect.smt.claimcheck.storage;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.*;
 
 import com.github.cokelee777.kafka.connect.smt.claimcheck.storage.s3.S3Storage;
 import org.apache.kafka.common.config.ConfigException;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("ClaimCheckStorageFactory 단위 테스트")
 public class ClaimCheckStorageFactoryTest {
 
-  private static final String TEST_CONFIG_STORAGE_TYPE = "s3";
-  private static final String TEST_CONFIG_UNSUPPORTED_STORAGE_TYPE = "UnsupportedStorage";
+  @Nested
+  @DisplayName("create 메서드 테스트")
+  class CreateTest {
 
-  @Test
-  @DisplayName("s3 타입이면 S3Storage를 생성한다")
-  void shouldCreateS3Storage() {
-    // when
-    ClaimCheckStorage storage = ClaimCheckStorageFactory.create(TEST_CONFIG_STORAGE_TYPE);
+    @Test
+    @DisplayName("올바른 Type을 인자로 넘겨주면 S3Storage 객체를 생성한다.")
+    public void rightType() {
+      // Given
+      String type = StorageType.S3.type();
 
-    // then
-    assertNotNull(storage);
-    assertTrue(storage instanceof S3Storage);
-    assertEquals(TEST_CONFIG_STORAGE_TYPE, storage.type());
-  }
+      // When
+      ClaimCheckStorage claimCheckStorage = ClaimCheckStorageFactory.create(type);
 
-  @Test
-  @DisplayName("지원하지 않는 타입이면 ConfigException이 발생한다")
-  void shouldThrowExceptionForUnsupportedType() {
-    // when & then
-    ConfigException exception =
-        assertThrows(
-            ConfigException.class,
-            () -> ClaimCheckStorageFactory.create(TEST_CONFIG_UNSUPPORTED_STORAGE_TYPE));
+      // Then
+      assertThat(claimCheckStorage).isNotNull();
+      assertThat(claimCheckStorage).isInstanceOf(S3Storage.class);
+      assertThat(claimCheckStorage.type()).isEqualTo(type);
+    }
 
-    assertEquals(
-        "Unsupported storage type: " + TEST_CONFIG_UNSUPPORTED_STORAGE_TYPE,
-        exception.getMessage());
+    @ParameterizedTest
+    @ValueSource(strings = {"invalidType"})
+    @DisplayName("지원하지 않는 Type을 인자로 넘기면 예외가 발생한다.")
+    public void invalidTypeCauseException(String type) { // Given
+      // When & Then
+      assertThatExceptionOfType(ConfigException.class)
+          .isThrownBy(() -> ClaimCheckStorageFactory.create(type))
+          .withMessage("Unsupported storage type: " + type);
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {" "})
+    @DisplayName("null 또는 빈 값의 Type을 인자로 넘기면 예외가 발생한다.")
+    public void nullOrBlankTypeCauseException(String type) { // Given
+      // When & Then
+      assertThatExceptionOfType(ConfigException.class)
+          .isThrownBy(() -> ClaimCheckStorageFactory.create(type))
+          .withMessage("Storage type must be provided");
+    }
   }
 }
--- core/src/test/java/com/github/cokelee777/kafka/connect/smt/claimcheck/storage/retry/RetryConfigTest.java
+++ core/src/test/java/com/github/cokelee777/kafka/connect/smt/claimcheck/storage/retry/RetryConfigTest.java
@@ -1,22 +1,25 @@
 package com.github.cokelee777.kafka.connect.smt.claimcheck.storage.retry;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import java.time.Duration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @DisplayName("RetryConfig 단위 테스트")
 class RetryConfigTest {
 
   @Nested
   @DisplayName("생성자 테스트")
-  class Constructor {
+  class ConstructorTest {
 
     @Test
     @DisplayName("올바른 인자를 세팅하면 정상적으로 객체가 생성된다.")
-    public void normalArgs() {
+    public void rightArgs() {
       // Given
       int maxAttempts = 3;
       Duration initialBackoff = Duration.ofMillis(300L);
@@ -32,46 +35,46 @@ class RetryConfigTest {
       assertThat(retryConfig.maxBackoff()).isSameAs(maxBackoff);
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(ints = {-1, -2, -3})
     @DisplayName("maxAttempts를 0보다 낮게 설정하면 예외가 발생한다.")
-    public void setMaxAttemptsLessThanZeroCauseException() {
-      // Given
-      int maxAttempts = -1;
+    public void setMaxAttemptsLessThanZeroCauseException(int maxAttempts) { // Given
       Duration initialBackoff = Duration.ofMillis(300L);
       Duration maxBackoff = Duration.ofMillis(20000L);
 
       // When & Then
-      assertThatThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
-          .isExactlyInstanceOf(IllegalArgumentException.class)
-          .hasMessage("maxAttempts must be >= 0");
+      assertThatExceptionOfType(IllegalArgumentException.class)
+          .isThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
+          .withMessage("maxAttempts must be >= 0");
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(longs = {-100L, -200L, -300L})
     @DisplayName("initialBackoff를 0보다 낮게 설정하면 예외가 발생한다.")
-    public void setInitialBackoffLessThanZeroCauseException() {
+    public void setInitialBackoffLessThanZeroCauseException(long initialBackoffMs) {
       // Given
       int maxAttempts = 3;
-      Duration initialBackoff = Duration.ofMillis(-300L);
+      Duration initialBackoff = Duration.ofMillis(initialBackoffMs);
       Duration maxBackoff = Duration.ofMillis(20000L);
 
       // When & Then
-      assertThatThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
-          .isExactlyInstanceOf(IllegalArgumentException.class)
-          .hasMessage("initialBackoff must be > 0");
+      assertThatExceptionOfType(IllegalArgumentException.class)
+          .isThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
+          .withMessage("initialBackoff must be > 0");
     }
 
-    @Test
+    @ParameterizedTest
+    @ValueSource(longs = {-10000L, -20000L, -30000L})
     @DisplayName("maxBackoff 0보다 낮게 설정하면 예외가 발생한다.")
-    public void setMaxBackoffLessThanZeroCauseException() {
-      // Given
+    public void setMaxBackoffLessThanZeroCauseException(long maxBackoffMs) { // Given
       int maxAttempts = 3;
       Duration initialBackoff = Duration.ofMillis(300L);
-      Duration maxBackoff = Duration.ofMillis(-20000L);
+      Duration maxBackoff = Duration.ofMillis(maxBackoffMs);
 
       // When & Then
-      assertThatThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
-          .isExactlyInstanceOf(IllegalArgumentException.class)
-          .hasMessage("maxBackoff must be > 0");
+      assertThatExceptionOfType(IllegalArgumentException.class)
+          .isThrownBy(() -> new RetryConfig(maxAttempts, initialBackoff, maxBackoff))
+          .withMessage("maxBackoff must be > 0");
     }
   }
 }
